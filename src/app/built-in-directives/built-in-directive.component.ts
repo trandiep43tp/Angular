3d83389ad5fa36fcd864f28f6636f12f4ccf7776
Tran Diep <contact@trandiep.com>
2017-10-30 00:01:57 +0700

    de mo

--- src/app/built-in-directives/built-in-directive.component.ts
+++ src/app/built-in-directives/built-in-directive.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: "app-built",
-    templateUrl: "./built-in-directive.component.html"
+    templateUrl: "./built-in-directive.component.html",
+    styleUrls: ["./built-in-directive.component.css"]
 })
 
 export class builtindirective{
@@ -11,7 +12,51 @@ export class builtindirective{
 
      //viết cho Ng For
      courses:string[]=["C","PHP","java"];
+
+     //viết cho Ng Swicth
+     lua_chon:number= 2;
+     tab:string="";
+
+
+     tab_click(e):void{
+       // console.log(e.target.text);
+      this.tab=e.target.text;  
+    }
     
+    //viết cho Ng class
+    canSave:Boolean=false;
+    isUnchanged:boolean=false;
+    isSpecial:boolean=true;
+
+    
+    currentClasses() {      
+      return  {
+        'aoxanh': this.canSave,
+        'aovang': this.isUnchanged,
+        'aodo':  this.isSpecial
+      };
+    }
+
+    /*
+    currentClasses1: {};
+    setCurrentClasses1() {
+      
+      this.currentClasses1 =  {          
+            'aoxanh': this.canSave,
+            'aovang': this.isUnchanged,
+            'aodo':  this.isSpecial           
+       };      
+    }
+    */
 
 
+    //viết cho ng Style    
+    currentStyles() {      
+      return {
+        'font-style':  this.canSave      ? 'italic' : 'normal',
+        'font-weight': this.isUnchanged ? 'bold'   : 'normal',
+        'font-size':   !this.isSpecial    ? '24px'   : '12px',
+        'color':        this.isSpecial ? 'red' : 'pink'
+      };
+    }
 }
